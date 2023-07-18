d610c98c55738578dc775caae6f62152cd967220
romanhalaichuk <vi2virbien@gmail.com>
2023-07-18 10:26:30 +0200

    modal

--- js/footer-modal.js
+++ js/footer-modal.js
@@ -1,7 +1,14 @@
+// document.getElementById("openModalBtn").addEventListener("click", function () {
+//   document.getElementById("modal").classList.add("is-open");
+// });
+
+// document.getElementById("closeModalBtn").addEventListener("click", function () {
+//   document.getElementById("modal").classList.remove("is-open");
+// });
 document.getElementById("openModalBtn").addEventListener("click", function () {
-  document.getElementById("modal").classList.add("is-open");
+  document.getElementById("modal").style.display = "flex";
 });
 
 document.getElementById("closeModalBtn").addEventListener("click", function () {
-  document.getElementById("modal").classList.remove("is-open");
+  document.getElementById("modal").style.display = "none";
 });
